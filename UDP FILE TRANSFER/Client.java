86037f3608e17ec113176a3944780d4782c96f65
Rageshwaran-HR <118850069+Rageshwaran-HR@users.noreply.github.com>
2024-10-21 08:41:41 +0530

    new change

--- UDP FILE TRANSFER/Client.java
+++ UDP FILE TRANSFER/Client.java
@@ -1,59 +1,41 @@
 import java.io.*;
 import java.net.*;
 
-public class FileReceiver {
+public class UDPClient {
 
-  public static void main(String[] args) {
-    DatagramSocket socket = null;
-    try {
-      socket = new DatagramSocket(9876); // Bind to port 9876
-      byte[] buffer = new byte[1024]; // Buffer to store received packets
+  public static void main(String[] args) throws IOException {
+    DatagramSocket socket = new DatagramSocket();
+    InetAddress IPAddress = InetAddress.getByName("localhost");
+    byte[] sendData = new byte[1024];
 
-      System.out.println("Waiting for file...");
+    FileInputStream fis = new FileInputStream("file_to_send.txt");
+    System.out.println("Sending the file...");
 
-      // Receive file name first
-      DatagramPacket fileNamePacket = new DatagramPacket(buffer, buffer.length);
-      socket.receive(fileNamePacket);
-      String fileName = new String(
-        fileNamePacket.getData(),
-        0,
-        fileNamePacket.getLength()
+    // Reading file in chunks and sending
+    int bytesRead;
+    while ((bytesRead = fis.read(sendData)) != -1) {
+      DatagramPacket sendPacket = new DatagramPacket(
+        sendData,
+        bytesRead,
+        IPAddress,
+        9876
       );
-
-      // Create a file to store the received content
-      FileOutputStream fileOutputStream = new FileOutputStream(
-        "received_" + fileName
-      );
-      System.out.println("Receiving file: " + fileName);
-
-      // Receive the file content
-      while (true) {
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-        socket.receive(packet);
-
-        // Check for termination message
-        String receivedMessage = new String(
-          packet.getData(),
-          0,
-          packet.getLength()
-        );
-        if (receivedMessage.equals("END")) {
-          System.out.println("File transfer completed.");
-          break;
-        }
-
-        // Write the received packet data to the file
-        fileOutputStream.write(packet.getData(), 0, packet.getLength());
-        System.out.println("Received packet of size: " + packet.getLength());
-      }
-
-      fileOutputStream.close();
-    } catch (IOException e) {
-      System.out.println("Error in receiving file: " + e.getMessage());
-    } finally {
-      if (socket != null && !socket.isClosed()) {
-        socket.close();
-      }
+      socket.send(sendPacket);
     }
+
+    // Send end of file signal
+    String endSignal = "END";
+    sendData = endSignal.getBytes();
+    DatagramPacket endPacket = new DatagramPacket(
+      sendData,
+      sendData.length,
+      IPAddress,
+      9876
+    );
+    socket.send(endPacket);
+
+    System.out.println("File sent successfully.");
+    fis.close();
+    socket.close();
   }
 }
--- UDP FILE TRANSFER/Server.java
+++ UDP FILE TRANSFER/Server.java
@@ -1,62 +1,37 @@
 import java.io.*;
 import java.net.*;
 
-public class FileSender {
+public class UDPServer {
 
-  public static void main(String[] args) {
-    DatagramSocket socket = null;
-    try {
-      InetAddress receiverAddress = InetAddress.getByName("localhost"); // Receiver's address
-      socket = new DatagramSocket(); // Create UDP socket
+  public static void main(String[] args) throws IOException {
+    DatagramSocket socket = new DatagramSocket(9876);
+    byte[] receiveData = new byte[1024];
 
-      // File to send
-      File file = new File("file_to_send.txt");
-      FileInputStream fileInputStream = new FileInputStream(file);
-      byte[] buffer = new byte[1024]; // Buffer to store chunks of the file
+    System.out.println("Server is ready to receive the file...");
+    FileOutputStream fos = new FileOutputStream("received_file.txt");
 
-      System.out.println("Sending file: " + file.getName());
-
-      // Send the file name first
-      byte[] fileNameBytes = file.getName().getBytes();
-      DatagramPacket fileNamePacket = new DatagramPacket(
-        fileNameBytes,
-        fileNameBytes.length,
-        receiverAddress,
-        9876
+    // Loop to receive all the file data
+    while (true) {
+      DatagramPacket receivePacket = new DatagramPacket(
+        receiveData,
+        receiveData.length
       );
-      socket.send(fileNamePacket);
-
-      // Send the file content
-      int bytesRead;
-      while ((bytesRead = fileInputStream.read(buffer)) != -1) {
-        DatagramPacket packet = new DatagramPacket(
-          buffer,
-          bytesRead,
-          receiverAddress,
-          9876
-        );
-        socket.send(packet); // Send each chunk
-        System.out.println("Sent packet of size: " + bytesRead);
-      }
-
-      // Send a termination message to indicate the file transfer is complete
-      String endMessage = "END";
-      DatagramPacket endPacket = new DatagramPacket(
-        endMessage.getBytes(),
-        endMessage.getBytes().length,
-        receiverAddress,
-        9876
+      socket.receive(receivePacket);
+      String received = new String(
+        receivePacket.getData(),
+        0,
+        receivePacket.getLength()
       );
-      socket.send(endPacket);
-      System.out.println("File transfer completed.");
 
-      fileInputStream.close();
-    } catch (IOException e) {
-      System.out.println("Error in sending file: " + e.getMessage());
-    } finally {
-      if (socket != null && !socket.isClosed()) {
-        socket.close();
+      // End of file transfer signal
+      if (received.equals("END")) {
+        System.out.println("File transfer complete.");
+        break;
       }
+      fos.write(receivePacket.getData(), 0, receivePacket.getLength());
     }
+
+    fos.close();
+    socket.close();
   }
 }
